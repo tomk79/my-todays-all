790f92759e89c224756261a8dd52b8a17dc1d85a
Tomoya Koyanagi <tomk79@gmail.com>
2016-04-30 21:44:31 +0900

    polymer: iron-tabs のタブ選択イベント処理を追加

--- src/common/scripts/main.js
+++ src/common/scripts/main.js
@@ -1,47 +1,24 @@
 (function(window){
 	var $ = window.$ = require('jquery');
 	var remote = require('remote');
-	var diffdir = remote.require('./node/diffdir.js');
 	var dialog = remote.require('dialog');
 	var browserWindow = remote.require('browser-window');
 	// console.log(diffdir);
 
 	window.main = new (function(){
-		this.executeDiffdir = function(form){
-			var $form = $(form);
-			var dd = new diffdir(
-				$form.find('input[name=before]').val() ,
-				$form.find('input[name=after]').val() ,
-				{
-					'-o': $form.find('input[name=output]').val() ,
-					'--strip-crlf': $form.find('input[name=strip-crlf]:checked').val()
-				}
-			);
-			$('input,button').attr({
-				'disabled': 'disabled'
-			});
-			dd.execute(function(data, error, code){
-				alert('fin.');
-				$('input,button').removeAttr('disabled');
-			});
-		}
-
-
 	})();
 
 	$(window).load(function(){
-		$('.select-local-directory').on('click', function(){
-			var $inputFor = $($(this).attr('data-for'));
-			var focusedWindow = browserWindow.getFocusedWindow();
-			dialog.showOpenDialog(focusedWindow, {
-				properties: ['openDirectory']
-			}, function(directories){
-				directories.forEach(function(directory){
-					$inputFor.val(directory);
-					// console.log(directory);
-				});
-			});
-		});
+		$('paper-tabs')
+			.bind('iron-select', function(e){
+				// console.log('selected!');
+				// console.log(e);
+				var $selected = $(this).find('paper-tab.iron-selected');
+				// console.log($selected.attr('data-tab-name'));
+				$('[data-tab-content]').hide();
+				$('[data-tab-content='+$selected.attr('data-tab-name')+']').show();
+			})
+		;
 
 	});
 
