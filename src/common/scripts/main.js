d37b64bce8d6118d0d98445c8c3d1ce61b69dcd9
Tomoya Koyanagi <tomk79@gmail.com>
2016-05-03 11:03:43 +0900

    add polymer element: paper-listbox, paper-item, paper-dialog-scrollable

--- src/common/scripts/main.js
+++ src/common/scripts/main.js
@@ -7,6 +7,35 @@
 
 	window.main = new (function(){
 		this.desktopUtils = remote.require('desktop-utils');
+		this.openSettings = function(){
+			var elm = document.getElementById('settings');
+			elm.open();
+			document.querySelector('iron-list').items = [
+				{
+					"name": "item1",
+					"longText": "long text."
+				} ,
+				{
+					"name": "item2",
+					"longText": "long text."
+				} ,
+				{
+					"name": "item3",
+					"longText": "long text."
+				} ,
+				{
+					"name": "item4",
+					"longText": "long text."
+				} ,
+				{
+					"name": "item5",
+					"longText": "long text."
+				}
+			];
+			setTimeout(function(){
+				elm.fit();
+			}, 100);
+		}
 	})();
 
 	$(window).load(function(){
@@ -20,29 +49,6 @@
 				$('[data-tab-content='+$selected.attr('data-tab-name')+']').show();
 			})
 		;
-		$('iron-list').get(0).items = [
-			{
-				"name": "item1",
-				"longText": "long text."
-			} ,
-			{
-				"name": "item2",
-				"longText": "long text."
-			} ,
-			{
-				"name": "item3",
-				"longText": "long text."
-			} ,
-			{
-				"name": "item4",
-				"longText": "long text."
-			} ,
-			{
-				"name": "item5",
-				"longText": "long text."
-			}
-		];
-		document.getElementById('settings').fitInto = $('body').get(0);
 
 	});
 
