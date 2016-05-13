a55a9c219f653cc24128367f71c5aa50a397d4ff
Tomoya Koyanagi <tomk79@gmail.com>
2016-05-13 12:10:31 +0900

    menuの実装をアプリ内部に移植。

--- dist/common/scripts/init.js
+++ dist/common/scripts/init.js
@@ -24,6 +24,103 @@ module.exports = function( main, callback ){
 
 	});
 
+	(function () {
+		// menubar セットアップ
+		var electron = remote.require('electron');
+		var Menu = electron.Menu;
+		return;
+
+		// if (process.platform == 'darwin') {
+		// var name = require('electron').app.getName();
+
+		var menu;
+		if (process.platform == 'darwin') {
+			menu = Menu.buildFromTemplate([
+				{
+					label: 'Electron',
+					submenu: [
+						{
+							label: 'Quit',
+							accelerator: 'Command+Q',
+							click: function () { app.quit(); }
+						},
+					]
+				},
+				{
+					label: "Application",
+					submenu: [
+						{ label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+						{ type: "separator" },
+						{ label: "Quit", accelerator: "Command+Q", click: function () { app.quit(); } }
+					]
+				},
+				{
+					label: 'View',
+					submenu: [
+						{
+							label: 'Reload',
+							accelerator: 'Command+R',
+							click: function () { mainWindow.restart(); }
+						},
+						{
+							label: 'Toggle Full Screen',
+							accelerator: 'Ctrl+Command+F',
+							click: function () { mainWindow.setFullScreen(!mainWindow.isFullScreen()); }
+						},
+						{
+							label: 'Toggle Developer Tools',
+							accelerator: 'Alt+Command+I',
+							click: function () { mainWindow.toggleDevTools(); }
+						},
+					]
+				},
+				{
+					label: "Edit",
+					submenu: [
+						{ label: "Undo", accelerator: "Command+Z", selector: "undo:" },
+						{ label: "Redo", accelerator: "Shift+Command+Z", selector: "redo:" },
+						{ type: "separator" },
+						{ label: "SelectAll", accelerator: "Command+A", selector: "selectall:" },
+						{ label: "Cut", accelerator: "Command+X", selector: "cut:" },
+						{ label: "Copy", accelerator: "Command+C", selector: "copy:" },
+						{ label: "Paste", accelerator: "Command+V", selector: "paste:" }
+					]
+				}
+			]);
+			Menu.setApplicationMenu(menu);
+		} else {
+			menu = Menu.buildFromTemplate([
+				{
+					label: '&View',
+					submenu: [
+						{
+							label: '&Reload',
+							accelerator: 'Ctrl+R',
+							click: function () { mainWindow.restart(); }
+						},
+						{
+							label: 'Toggle &Full Screen',
+							accelerator: 'F11',
+							click: function () { mainWindow.setFullScreen(!mainWindow.isFullScreen()); }
+						},
+						{
+							label: 'Toggle &Developer Tools',
+							accelerator: 'Alt+Ctrl+I',
+							click: function () { mainWindow.toggleDevTools(); }
+						},
+						{
+							label: 'Toggle &Developer Tools',
+							accelerator: 'F12',
+							click: function () { mainWindow.toggleDevTools(); }
+						},
+					]
+				}
+			]);
+			// mainWindow.setMenu(menu);
+			Menu.setApplicationMenu(menu);
+		}
+	})();
+
 	main.settings = new (require('./settings.js'))(main);
 	main.dbh = new (require(__dirname+'/dbh.js'))(main, function(){
 		main.accountMgr = new (require('./accountMgr.js'))(main, function(){
--- main.js
+++ main.js
@@ -22,100 +22,6 @@ app.on('window-all-closed', function() {
 var electron = require('electron');
 var Menu = electron.Menu;
 
-var installMenu = function () {
-	// 開発者ツール
-	// mainWindow.toggleDevTools();
-
-	// if (process.platform == 'darwin') {
-	// var name = require('electron').app.getName();
-
-	var menu;
-	if (process.platform == 'darwin') {
-		menu = Menu.buildFromTemplate([
-			{
-				label: 'Electron',
-				submenu: [
-					{
-						label: 'Quit',
-						accelerator: 'Command+Q',
-						click: function () { app.quit(); }
-					},
-				]
-			},
-			{
-				label: "Application",
-				submenu: [
-					{ label: "About Application", selector: "orderFrontStandardAboutPanel:" },
-					{ type: "separator" },
-					{ label: "Quit", accelerator: "Command+Q", click: function () { app.quit(); } }
-				]
-			},
-			{
-				label: 'View',
-				submenu: [
-					{
-						label: 'Reload',
-						accelerator: 'Command+R',
-						click: function () { mainWindow.restart(); }
-					},
-					{
-						label: 'Toggle Full Screen',
-						accelerator: 'Ctrl+Command+F',
-						click: function () { mainWindow.setFullScreen(!mainWindow.isFullScreen()); }
-					},
-					{
-						label: 'Toggle Developer Tools',
-						accelerator: 'Alt+Command+I',
-						click: function () { mainWindow.toggleDevTools(); }
-					},
-				]
-			},
-			{
-				label: "Edit",
-				submenu: [
-					{ label: "Undo", accelerator: "Command+Z", selector: "undo:" },
-					{ label: "Redo", accelerator: "Shift+Command+Z", selector: "redo:" },
-					{ type: "separator" },
-					{ label: "SelectAll", accelerator: "Command+A", selector: "selectall:" },
-					{ label: "Cut", accelerator: "Command+X", selector: "cut:" },
-					{ label: "Copy", accelerator: "Command+C", selector: "copy:" },
-					{ label: "Paste", accelerator: "Command+V", selector: "paste:" }
-				]
-			}
-		]);
-		Menu.setApplicationMenu(menu);
-	} else {
-		menu = Menu.buildFromTemplate([
-			{
-				label: '&View',
-				submenu: [
-					{
-						label: '&Reload',
-						accelerator: 'Ctrl+R',
-						click: function () { mainWindow.restart(); }
-					},
-					{
-						label: 'Toggle &Full Screen',
-						accelerator: 'F11',
-						click: function () { mainWindow.setFullScreen(!mainWindow.isFullScreen()); }
-					},
-					{
-						label: 'Toggle &Developer Tools',
-						accelerator: 'Alt+Ctrl+I',
-						click: function () { mainWindow.toggleDevTools(); }
-					},
-					{
-						label: 'Toggle &Developer Tools',
-						accelerator: 'F12',
-						click: function () { mainWindow.toggleDevTools(); }
-					},
-				]
-			}
-		]);
-		// mainWindow.setMenu(menu);
-		Menu.setApplicationMenu(menu);
-	}
-}
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -124,13 +30,6 @@ app.on('ready', function () {
 	mainWindow = new BrowserWindow({ width: 600, height: 470 });
 	// and load the index.html of the app.
 	mainWindow.loadUrl('file://' + __dirname + '/dist/index.html');
-
-
-	try {
-		installMenu();
-	} catch (e) {
-		console.log(e);
-	}
 
 	// Emitted when the window is closed.
 	mainWindow.on('closed', function () {
