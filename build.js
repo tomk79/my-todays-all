ce55df2a8178673d638a45a1c76216fdff88adef
Tomoya Koyanagi <tomk79@gmail.com>
2016-09-23 02:24:20 +0900

    ビルドスクリプト更新

--- build.js
+++ build.js
@@ -1,17 +1,47 @@
 /**
- * $ node build.js
+ * build.js
  */
 var packager = require('electron-packager');
-var config = require('./package.json');
 var zipFolder = require('zip-folder');
 var rimraf = require('rimraf');
+var utils79 = require('utils79');
 var Promise = require('es6-promise').Promise;
 
-function build( condition, callback ){
-	var dir = './';
-	var out = './build';
-	var name = config.name;
+var config = require('./package.json');
+var dir = './';
+var out = './build';
+var name = config.name;
 
+console.log('== build "'+name+'" ==');
+
+/**
+ * クリーニング
+ */
+function cleanup(callback){
+	callback = callback || function(){};
+	var fsX = require('fs-extra');
+
+	var ls = require('fs').readdirSync(out);
+	for(var idx in ls){
+		if( ls[idx] == '.gitkeep' ){continue;}
+		if( utils79.is_dir(out+'/'+ls[idx]) ){
+			console.log('dir: '+ls[idx]);
+			fsX.removeSync(out+'/'+ls[idx]);
+		}else if( utils79.is_file(out+'/'+ls[idx]) ){
+			console.log('file: '+ls[idx]);
+			fsX.removeSync(out+'/'+ls[idx]);
+		}
+	}
+
+	callback();
+	return;
+}
+
+/**
+ * ビルド
+ */
+function build( condition, callback ){
+	callback = callback || function(){};
 	var platform = condition.platform;
 	var arch = condition.arch;
 
@@ -29,7 +59,7 @@ function build( condition, callback ){
 		icon = './resources/app_for_Windows.ico'; //<- アプリアイコン
 	}
 
-	var version = '1.4.0';
+	var version = '1.4.1';
 	var app_bundle_id = 'jp.pxt.applications.my-todays-all'; //<- 自分のドメインなどを使用してください
 	var helper_bundle_id = 'jp.pxt.applications.my-todays-all'; //<- 自分のドメインなどを使用してください
 
@@ -51,7 +81,8 @@ function build( condition, callback ){
 			}
 		);
 	}
-	var npm_ignore = [], ignores;
+	var npm_ignore = [],
+	    ignores;
 	for (var key in config.devDependencies) {
 		npm_ignore.push(key);
 	}
@@ -70,12 +101,12 @@ function build( condition, callback ){
 			'app-bundle-id': app_bundle_id,
 			'app-version': config.version,
 			'helper-bundle-id': helper_bundle_id,
-			overwrite: true,
-			// asar: false,
-			asar: true, // aserに固める
+			"overwrite": true,
+			// "asar": false,
+			"asar": true, // aserに固める
 			"asar-unpack-dir": "unpacked",
-			prune: true,
-			ignore: 'node_modules/(' + ignores + '|\.bin)|build|build\.js|tests|src|submodules|materials|gulpfile.js|bower.json|.bowerrc',
+			"prune": true,
+			"ignore": 'node_modules/(' + ignores + '|\.bin)|build|build\.js|tests|src|submodules|materials|gulpfile.js|bower.json|.bowerrc',
 			'version-string': {
 				CompanyName: 'tomk79',
 				FileDescription: config.description,
@@ -105,6 +136,14 @@ function build( condition, callback ){
 
 
 new Promise(function(rlv){rlv();})
+	.then(function(){ return new Promise(function(rlv, rjt){
+		console.log('--- cleanup ---');
+		cleanup(function(){
+			console.log('done.');
+			console.log('');
+			rlv();
+		});
+	}); })
 	.then(function(){ return new Promise(function(rlv, rjt){
 		console.log('--- Building for Darwin x64 ---');
 		build({'platform':'darwin','arch':'x64'}, function(){
